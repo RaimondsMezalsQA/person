c70bcd6463f96dc6693dcf6030c22f48da389d5b
RaimondsMezalsQA <RMezals@qa.com>
2021-01-11 14:37:11 +0000

    finished person up to task 5

---
+++ src/com/qa/main/PersonList.java
@@ -0,0 +1,51 @@
+package com.qa.main;
+
+import java.util.ArrayList;
+//import java.util.List;
+
+public class PersonList {
+	
+	Person personL = new Person();
+	
+	public ArrayList<Person> personList = new ArrayList<>();
+	
+	public void setList(Person input) {
+		
+		personList.add(input);
+		
+	}
+	 public void getList() {
+		 
+			System.out.println(personList);
+		 
+	 }
+	 
+	 public void searchList(int input) {
+		 
+		 System.out.println(personList.get(input));
+		 
+	 }
+	
+	 public void searchByName(String inputName) {
+		 
+		 
+		 for(Person i : personList) {
+			 
+			 String typedPerson = inputName;
+			 String currentPerson = i.getName();
+			 
+			 //System.out.println(currentPerson);
+			 
+			 if(typedPerson.equals(currentPerson)) {
+				 
+				 System.out.println(i);
+				 
+			 }
+			 
+		 }
+		 
+		 
+	 }
+	
+	
+}
---
+++ src/com/qa/main/Runner.java
@@ -0,0 +1,48 @@
+package com.qa.main;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		Person person0 = new Person();
+		
+		System.out.println(person0.toString());
+		
+		Person person1 = new Person();
+		
+		person1.setName("Josh");
+		person1.setAge(34);
+		person1.setJobTitle("Solicitor");
+		
+Person person2 = new Person();
+		
+		person2.setName("Carl");
+		person2.setAge(32);
+		person2.setJobTitle("Manager");
+		
+Person person3 = new Person();
+		
+		person3.setName("Cloe");
+		person3.setAge(23);
+		person3.setJobTitle("Director");
+		
+Person person4 = new Person();
+		
+		person4.setName("Leo");
+		person4.setAge(21);
+		person4.setJobTitle("QA Consultant");
+		
+		
+		PersonList listP = new PersonList();
+		
+		listP.setList(person1);
+		listP.setList(person2);
+		listP.setList(person3);
+		listP.setList(person4);
+		
+		listP.getList();
+		listP.searchByName("Leo");
+		
+		
+	}
+}
